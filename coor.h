7f7b4dca9931955d9243c23bfd8c6b865e7c26a6
justapig9020 <justapig9020@gmail.com>
2020-05-29 12:24:28 +0800

    rebuild game set; define game next step

--- coor.h
+++ coor.h
@@ -1,13 +1,16 @@
 #ifndef _FUND_H_
 #define _FUND_H_
 
+#define ADJ_SZ 26
 class Coordinate
 {
-    public:
+    private:
         int x;
         int y;
         int z;
+    public:
         Coordinate(int, int, int);
+        get_adjs();
 };
 // TODO overide equal
 
