570561e357d9acb5ae7eaeae7038dd707c25b4f9
justapig9020 <justapig9020@gmail.com>
2020-05-30 00:50:02 +0800

    blocks constructor and util

--- block.cpp
+++ block.cpp
@@ -1,8 +1,19 @@
 #include "block.h"
 
-/*
-Block::Block(Coordinate pst)
+Block::Block(Coordinate posi)
 {
-
+    this->coor = posi;
 }
-*/
+
+Coordinate get_coor()
+{
+    return this->coor;
+}
+
+Empty::Empty(Coordinate posi) : Block(posi);
+
+Koz::Koz(Coordinate posi) : Block(posi);
+
+Path::Path(Coordinate posi, Block *prev, dist_t g, dist_t h) : Block(posi)
+{
+}
